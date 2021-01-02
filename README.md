5f5d9377c4088069e707edb62efb6592f2565a0d
Danny H <git@bean.codes>
2021-01-02 07:37:40 -0800

    BIRTHDAY AND WEATHER OVERHAUL (ALSO GREETINGS!)

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Miniboard
-A standalone Electron program displaying the time and some other statistics. A fork of SmatMan's [personal dashboard](https://github.com/SmatMan/personal_dashboard), but with Node.js internals instead of Python.
+A program written in Node.js using the Electron framework to display the weather, the time, and the date. It also allows you to use the version of YouTube used on Smart TVs (not to be confused with YouTube TV, the subscription TV service).
+
+### How can I use Miniboard?
+**At the moment, Miniboard does not have a standalone binary, but these will be made soon.**
+
+You can download, configure, and run this in just a few steps.
+
+1. **Download Node.js and Electron if you haven't already done so.**
+
+You can install Node for Linux and macOS [here, following the instructions](https://github.com/nvm-sh/nvm), or you can install it on Windows [here](https://nodejs.org).
+
+2. Install Electron using the following command:
+
+``npm i -g electron``
+
+3. Clone the repository like so, then cd into the directory:
+
+``git clone https://github.com/Dannnington/miniboard``
+
+``cd miniboard``
+
+4. Download dependencies:
+
+``npm install``
+
+5. Run the app!
+
+``electron ./main.js``
+
+### What's the inspiration for this project? Was this forked?
+The original purpose of Miniboard was to have a little dashboard running on my Raspberry Pi (even though I already have my own Android TV in my room). This inspiration also ties in with if this was based off some other project. **Miniboard is based on a little [project](https://github.com/SmatMan/personal_dashboard) that one of my best mates created**, but while his dashboard is a **webserver written in Python**'s Flask engine, Miniboard is a **standalone Electron app** (albeit not compiled into a binary). The frontend UI is mostly similar, too, and Miniboard has some things that my friend's dashboard does not have (and vice versa).
+
+#### Should I use Miniboard or this other dashboard?
+It's personal preference, really. If you need/want a dashboard that can be installed on a computer, use Miniboard. If you need a webserver and/or a dashboard that can be used with a standard browser, go with [the personal dashboard](https://github.com/SmatMan/personal_dashboard).
+
+### PRs?
+Yes. I'm accepting PRs for this project.
--- src/script.js
+++ src/script.js
@@ -1,76 +1,98 @@
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const configuration = window.bridge.information.getConfig()
+
+function pickTz() {
+    if (configuration.decor["12hr"] == true) {
+        return new Date().toLocaleTimeString("en-US").replace(/(.*)\D\d+/, '$1');
+    } if (configuration.decor["12hr"] !== true) {
+        return new Date().toLocaleTimeString("en-GB").replace(/(.*)\D\d+/, '$1');
+    }
+};
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 function getTime() {
-    var d = new Date().toLocaleTimeString().replace(/(.*)\D\d+/, '$1');
+    var d = pickTz();
     document.getElementById("timeJS").innerHTML = d;
 }
 
-function getDayOfWeek() {
-    var now = new Date();
-    var day = days[now.getDay()];
-    console.log(day);
-    document.getElementById("dayname").innerHTML = day;
+function getGreeting() {
+    const name = configuration.decor.name
+    const hoursOfDay = {
+        "morning": [5, 6, 7, 8, 9, 10, 11],
+        "noon": [12],
+        "afternoon": [13, 14, 15, 16, 17],
+        "evening": [18, 19, 20, 21],
+        "night": [22, 23, 0, 1, 2, 3, 4]
+    };
+
+    const greetings = {
+        "morning": [`Good morning, ${name}!`, `Rise and shine, ${name}!`, `How's your morning ${name}?`, `Did you have a nice sleep ${name}?`, `When's breakfast, ${name}?`],
+        "noon": [`Howdy, ${name}, how's your lunch been?`, `Lunch-time, ${name}!`, `How was the food, ${name}?`],
+        "afternoon": [`Good afternoon, ${name}!`, `Have a good rest of your day, ${name}!`, `Good afternoon ${name}, when's dinner?`, `Quite hungry, ${name}, what about you?`],
+        "evening": [`Evenin', ${name}. How's your night been?`, `${name}, dinner time!`, `${name}, good evening!`],
+        "night": [`Good night, ${name}. Sleep well!`, `Good night ${name}.`, `Don't let the bed bugs bite, ${name}.`]
+    }
+
+    if (hoursOfDay.morning.includes(new Date().getHours())) {
+        document.getElementById("greeting").innerText = greetings.morning[Math.floor(Math.random() * greetings.morning.length)];
+    } if (hoursOfDay.noon.includes(new Date().getHours())) {
+        document.getElementById("greeting").innerText = greetings.noon[Math.floor(Math.random() * greetings.noon.length)];
+    } if (hoursOfDay.afternoon.includes(new Date().getHours())) {
+        document.getElementById("greeting").innerText = greetings.afternoon[Math.floor(Math.random() * greetings.afternoon.length)];
+    } if (hoursOfDay.evening.includes(new Date().getHours())) {
+        document.getElementById("greeting").innerText = greetings.evening[Math.floor(Math.random() * greetings.evening.length)];
+    } if (hoursOfDay.night.includes(new Date().getHours())) {
+        document.getElementById("greeting").innerText = greetings.night[Math.floor(Math.random() * greetings.night.length)];
+    }
 }
 
 function getDDMMYY() {
     const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const birthday = configuration.decor.birthday.toLowerCase().split(" ");
     const dateObj = new Date();
     const month = monthNames[dateObj.getMonth()];
-    const day = String(dateObj.getDate()).padStart(2, '0');
-    const year = dateObj.getFullYear();
-    const output = day + '\n' + month + ' ' + year;
-    document.getElementById("ddmmyy").innerHTML = output;
-    document.getElementById("daynamemid").innerHTML = output;
-}
-
-function middleDate() {
-    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    const dateObj = new Date();
-    const month = monthNames[dateObj.getMonth()];
-    const day = String(dateObj.getDate()).padStart(2, '0');
+    let day = String(dateObj.getDate());
     const year = dateObj.getFullYear();
     const dayforweek = days[dateObj.getDay()];
-    const output = dayforweek + ", " + day + '\n' + month + ' ' + year;
+    output = `${dayforweek}, ${day} ${month} ${year}`;
+    if (birthday[0] == day && birthday[1] == month.toLowerCase()) confetti.start()
     document.getElementById("daynamemid").innerHTML = output;
 }
 
 function weather() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?id=${configuration.placeid}&appid=${configuration.key}`)  
+    let units = {}
+    if (configuration.units == 0 || configuration.units == undefined) {
+        units.fc = "C"
+        units.unit = "metric"
+    }
+    if (configuration.units == 1) { 
+        units.fc = "F"
+        units.unit = "imperial"
+    }
+    fetch(`https://api.openweathermap.org/data/2.5/weather?id=${configuration.placeid}&appid=${configuration.key}&units=${units.unit}`)  
     .then(function(resp) { return resp.json() }) // Convert data to json
     .then(function(data) {
         document.getElementById("place").innerText = `${data.name}, ${data.sys.country}`
-        var celcius = Math.round(parseFloat(data.main.temp)-273.15);
-        var conditions = data.weather[0].description
-        document.getElementById('temp').innerHTML = celcius + '&deg;C';
+        var temperature = Math.round(parseFloat(data.main.temp));
+        var conditions = capitalizeFirstLetter(data.weather[0].description);
+        document.getElementById('temp').innerHTML = temperature + `&deg;${units.fc}`;
         document.getElementById('weatherdesc').innerHTML = conditions;
     })
     .catch(e => console.log(e));
 }
 
-function reloadCSS() {
-    var links = document.getElementsByTagName("link");
-
-    for (var x in links) {
-        var link = links[x];
-    
-        if (link.getAttribute("type").indexOf("css") > -1) {
-            link.href = link.href + "?id=" + new Date().getMilliseconds();
-        }
-    }
-}
-
-
 window.onload = function() {
     weather();   
-    getDayOfWeek();
     getTime();
     getDDMMYY();
-    middleDate();
+    getGreeting();
 }
 
 setInterval(getTime, 1000);
-setInterval(getDDMMYY(), 10000)
-setInterval(getDayOfWeek(), 1000)
-setInterval(reloadCSS, 120000);
+setInterval(getGreeting, 600000);
+setInterval(getDDMMYY, 10000);
 setInterval(weather, 120000);
 document.body.style.background = `url(${configuration.decor.background}) no-repeat center center`;
