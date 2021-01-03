8d923c711c5974730bf9f154fa4521b8581f06f9
Danny H <git@bean.codes>
2021-01-03 03:02:28 -0800

    I FORGOT SUDO

--- preload.js
+++ preload.js
@@ -30,9 +30,8 @@ contextBridge.exposeInMainWorld(
                 }).catch(e => { return false; })
             },
             openNetflix: async () => {
-                const netflix = child.execFile("sh", ["/usr/local/bin/chromium-armhf"]);
+                const netflix = child.execFile("sudo sh", ["/usr/local/bin/chromium-armhf"]);
                 ipcRenderer.send("hideApp");
-                netflix.on("message", function (a) { console.log(a) })
                 netflix.on("exit", async function () {
                     ipcRenderer.send("showApp");
                 });
